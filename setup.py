ef7d61c7b72339c3c1012f48536c34858d1364de
Kristóf <163408857+BanyiKr@users.noreply.github.com>
2025-05-17 10:44:44 +0200

    Update setup.py with simulation folders

--- setup.py
+++ setup.py
@@ -14,7 +14,11 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         # Example: Include launch files if you have them
-        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*')))
+        (os.path.join('share', package_name, 'rviz'), glob('rviz/*.rviz')),
+        (os.path.join('share', package_name, 'worlds'), glob('worlds/*.world')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+        (os.path.join('share', package_name, 'gazebo_models'), glob('gazebo_models/*')),
         (os.path.join('share', package_name, 'config'), glob('config/*.yaml'))
     ],
     # Python requires
